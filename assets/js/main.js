b838b1369b87789a55695c147112dd1b101b346f
ShaniaMtz <sxm210198@utdallas.edu>
2026-04-07 19:29:08 -0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+// main.js
+// main.js
+
+// create a variable to store updates to rotation
+let rotation = 0;
+// add a click listener to the whole document
+document.addEventListener("click", function () {
+    // variables to store random min/max values
+    let min = 50;
+    let max = 100;
+    // update rotation
+    rotation += Math.floor(Math.random() * (max - min + 1)) + min;
+    // apply rotation
+    document.body.style.transform = `rotate(${rotation}deg)`;
+});
